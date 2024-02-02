8e80696e6afe13c8077b4bba202a0d7e9ae2041b
Beasty <infont404@gmail.com>
2024-02-02 18:06:26 +0400

    Add project files.

---
+++ NonRecursiveFolderReader/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace NonRecursiveFolderReader
+{
+    internal class Program
+    {
+        public static void ReadDirectories(string path)
+        {
+            Stack<string> avalibleDirectories = new Stack<string>();
+            avalibleDirectories.Push(path);
+
+            while (avalibleDirectories.Count > 0)
+            {
+                var currentPath = avalibleDirectories.Pop();
+                Console.WriteLine(currentPath);
+
+                foreach (var directory in Directory.GetDirectories(currentPath))
+                {
+                    avalibleDirectories.Push(directory);
+                }
+            }
+        }
+
+        public static void Main(String[] args)
+        {
+            ReadDirectories("C:\\Users\\user\\Downloads\\MusicCS");
+        }
+    }
+}
